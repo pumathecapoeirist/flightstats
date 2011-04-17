fdd645b247cc54902a48d7f367354f708948c247
pumathecapoeirist <puma@example.com>
2011-04-17 03:02:59 -0400

    now able to retrieve lowest price using selenium 2.0b3

--- src/main/java/kayak/KayakSearchFlightsPage.java
+++ src/main/java/kayak/KayakSearchFlightsPage.java
@@ -3,19 +3,65 @@ package kayak;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.DefaultElementLocator;
+import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
+import org.openqa.selenium.support.pagefactory.ElementLocator;
 
 public class KayakSearchFlightsPage {
 
     private static WebDriver driver;
     private static final String kayakURL = "http://www.kayak.com";
+    
+    
+    @FindBy(id = "origin")
+    private WebElement originWeb;
     private String origin;
+    
+
+    @FindBy(id = "destination")
+    private WebElement destinationWeb;
     private String destination;
+    
+    @FindBy(id = "depart_date")
+    private WebElement departDateWeb;
     private String departDate;
+    
+    @FindBy(id = "return_date")
+    private WebElement returnDateWeb;
     private String returnDate;
 
+    @FindBy(id = "cbEXPEDIA_DFDCMP2")
+    WebElement expediaCheckBox;
+
     public KayakSearchFlightsPage(WebDriver driver) {
-	this.driver = driver;
+	KayakSearchFlightsPage.driver = driver;
+    }
+
+    public static WebDriver getDriver() {
+	return driver;
+    }
+
+    public static String getKayakurl() {
+	return kayakURL;
+    }
+
+    public String getOrigin() {
+	return origin;
+    }
+
+    public String getDestination() {
+	return destination;
+    }
+
+    public String getDepartDate() {
+	return departDate;
+    }
+
+    public String getReturnDate() {
+	return returnDate;
     }
 
     public KayakSearchFlightsPage from(String origin) {
@@ -44,42 +90,42 @@ public class KayakSearchFlightsPage {
 		|| returnDate == null) {
 	    System.out.println(" YOU NEED TO INITIALIZE THE FLIGHT FIRST! ");
 	}
+	
+	DefaultElementLocatorFactory elmntLocFac = new DefaultElementLocatorFactory(
+		driver);
+	PageFactory.initElements(elmntLocFac, this);
 
 	// And now use this to visit the site
 	driver.get(kayakURL);
 
-	// Find the text input element by its id
-	WebElement element = driver.findElement(By.id("origin"));
+	// Enter something to search for
+	originWeb.sendKeys(origin);
+	
+	// Enter something to search for
+	destinationWeb.sendKeys(destination);
 
 	// Enter something to search for
-	element.sendKeys(origin);
-
-	// Find the text input element by its id
-	element = driver.findElement(By.id("destination"));
-
+	departDateWeb.sendKeys(departDate);
+	
 	// Enter something to search for
-	element.sendKeys(destination);
-
-	// Find the text input element by its id
-	element = driver.findElement(By.id("depart_date"));
-
-	// Enter something to search for
-	element.sendKeys(departDate);
-
-	// Find the text input element by its id
-	element = driver.findElement(By.id("return_date"));
-
-	// Enter something to search for
-	element.sendKeys(returnDate);
-
+	returnDateWeb.sendKeys(returnDate);
+	
+	
+	String s = expediaCheckBox.getAttribute("checked");
+	if (s.equals("true")) {
+	    expediaCheckBox.click();
+	}
+	
+	
+	// Find the boutton input element by its id
+	WebElement element = driver.findElement(By.id("fdimgbutton"));
+	
 	// Now submit the form. WebDriver will find the form for us from the
 	// element
 	element.submit();
 
 	// Check the title of the page
-	System.out.println("Page title is: " + driver.getTitle());
+	System.out.println("Submitting page");
 
-	// Close the browser
-	driver.quit();
     }
 }
--- src/main/java/kayak/Main.java
+++ src/main/java/kayak/Main.java
@@ -5,11 +5,20 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class Main {
-  public static void main(String[] args) {
-    WebDriver driver = new FirefoxDriver();
-    KayakSearchFlightsPage k = new KayakSearchFlightsPage(driver);
-    k.from("YUL").departDate("04/06/2011").to("PARIS").returnDate("05/06/2011");
-    PageFactory.initElements(driver, k);
-    k.launchSearch();
-  }
+    public static void main(String[] args) {
+	WebDriver driver = new FirefoxDriver();
+	
+	//Submit trip request
+	KayakSearchFlightsPage searchPage = new KayakSearchFlightsPage(driver);
+	searchPage.from("YUL").departDate("04/06/2011");
+	searchPage.to("NYC").returnDate("05/07/2011");
+	searchPage.launchSearch();
+	
+	//Get lowest price
+	KayakResultsFlightsPage resultPage = new KayakResultsFlightsPage(searchPage);
+	resultPage.getLowerPrice();
+	
+	// Close the browser
+	driver.quit();
+    }
 }
