76913430c19bc435701e3ffb3dfeb3d462ef0dd9
rdjondo <rdjondo@cscanada.ca>
2011-05-06 13:26:30 -0400

    The webdriver is now headless.

--- src/main/java/kayak/KayakSearchFlightsPage.java
+++ src/main/java/kayak/KayakSearchFlightsPage.java
@@ -87,7 +87,7 @@ public class KayakSearchFlightsPage {
 	// Enter something to search for
 	returnDateWeb.sendKeys(returnDate);
 	
-	//Uncked Expedia box that is checked by default
+	//Uncheck Expedia box if checked (the box is check by default)
 	String s = expediaCheckBox.getAttribute("checked");
 	if (s.equals("true")) {
 	    expediaCheckBox.click();
--- src/main/java/kayak/Main.java
+++ src/main/java/kayak/Main.java
@@ -2,11 +2,19 @@ package kayak;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 
 public class Main {
     public static void main(String[] args) {
-	WebDriver driver = new FirefoxDriver();
-	
+    	
+    //Creating a headless web driver
+    DesiredCapabilities caps = DesiredCapabilities.firefox(); 
+    caps.setJavascriptEnabled(true); 
+    WebDriver driver = new HtmlUnitDriver(caps); 
+    	
 	//Submit trip request
 	KayakSearchFlightsPage searchPage = new KayakSearchFlightsPage(driver);
 	searchPage.from("YUL").departDate("04/06/2011");
