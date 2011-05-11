13cd747d77f0c3f191a949d7326c71d075eb4e67
pumathecapoeirist <puma@example.com>
2011-05-11 00:09:21 -0400

    minor edition - comments and added init function signature for database

--- src/main/java/database/FligthDatabase.java
+++ src/main/java/database/FligthDatabase.java
@@ -1,5 +1,24 @@
 package database;
 
 public abstract class FligthDatabase {
+    
+    /**
+     * Clique as the graphs
+     */
+    static final String CLIQUE_CITIES[] = {
+	"PAR",
+	"YUL",
+	"NYC"
+    };
+    
+
+    static final String SECONDARY_CITIES[] = {
+	"PEK",
+	"TYO",
+	"SIN"
+    };
+    
+    abstract public void open();
     abstract public void close();
+    abstract public void isInit();
 }
--- src/main/java/database/H2FlightDatabase.java
+++ src/main/java/database/H2FlightDatabase.java
@@ -7,11 +7,15 @@ import org.h2.jdbcx.JdbcConnectionPool;
 public class H2FlightDatabase extends FligthDatabase {
     private Connection conn;
     private JdbcConnectionPool cp;
-    
+
     public H2FlightDatabase() {
+	
+    }
+
+    @Override
+    public void open() {
 	try {
-	    cp = JdbcConnectionPool.create(
-		    "jdbc:h2:test_database", "sa", "sa");
+	    cp = JdbcConnectionPool.create("jdbc:h2:test_database", "sa", "sa");
 	    conn = cp.getConnection();
 	    conn.close();
 	    cp.dispose();
@@ -21,6 +25,7 @@ public class H2FlightDatabase extends FligthDatabase {
 	}
     }
 
+    @Override
     public void close() {
 	try {
 	    conn.close();
@@ -29,6 +34,18 @@ public class H2FlightDatabase extends FligthDatabase {
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
+    }
+
+    @Override
+    public void isInit() {
+	// TODO Need to verify if database is well initialised with proper tables
+	// The database is unnormalised as possible and the tables are not connected
+	// Table1: Clique_cities
+	// Table2: Secondary_cities
+	// Table3: Everything: Origin - Destination - Date1 - Date2 - WebEngine - 
+	// Requests - Which date of the week is preferable for buying a ticket for this destination?
+	// 
+	
     }
 
 }
